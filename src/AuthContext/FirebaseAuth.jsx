8e8e1cc791e7df968c005aca8606ca27fce90c4e
Arish089 <152006719+Arish089@users.noreply.github.com>
2024-05-11 23:38:37 +0530

    done

--- src/AuthContext/FirebaseAuth.jsx
+++ src/AuthContext/FirebaseAuth.jsx
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
-const apiKey=import.meta.env.VITE_FIREBASE_FIREBASE_API_KEY
-const authDomain=import.meta.env.VITE_FIREBASE_FIREBASE_AUTH_DOMAIN
-const projectId=import.meta.env.VITE_FIREBASE_FIREBASE_PROJECT_ID
-const storageBucket=import.meta.env.VITE_FIREBASE_FIREBASE_STORAGE_BUCKET
-const messagingSenderId=import.meta.env.VITE_FIREBASE_FIREBASE_MESSAGING_SENDER_ID
-const appId=import.meta.env.VITE_FIREBASE_FIREBASE_APP_ID
+const apiKey = import.meta.env.VITE_FIREBASE_FIREBASE_API_KEY
+const authDomain = import.meta.env.VITE_FIREBASE_FIREBASE_AUTH_DOMAIN
+const projectId = import.meta.env.VITE_FIREBASE_FIREBASE_PROJECT_ID
+const storageBucket = import.meta.env.VITE_FIREBASE_FIREBASE_STORAGE_BUCKET
+const messagingSenderId = import.meta.env.VITE_FIREBASE_FIREBASE_MESSAGING_SENDER_ID
+const appId = import.meta.env.VITE_FIREBASE_FIREBASE_APP_ID
 
 
 const firebaseConfig = {
